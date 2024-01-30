89df17e5fb4ae8de1ee9bcc9160e3d9f8bfed63b
jayjang0209 <jayjang0209@gmail.com>
2024-01-30 14:43:51 -0800

    add clerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ZenMelody",
-  description: "SaaS app designed for generating music for study and meditation",
+  description:
+    "SaaS app designed for generating music for study and meditation",
 };
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
