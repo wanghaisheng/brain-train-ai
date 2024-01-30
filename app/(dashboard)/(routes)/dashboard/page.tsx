90a1bcab6f5fec3b1bba5ad64a9ce85166610828
jayjang0209 <jayjang0209@gmail.com>
2024-01-30 15:05:45 -0800

    add user button

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { UserButton } from '@clerk/nextjs'
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage Protected</div>
+    <div>DashboardPage Protected
+      <div>
+        <UserButton />
+      </div>
+    </div>
   )
 }
