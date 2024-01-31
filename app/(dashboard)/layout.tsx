230bfdec3743b8929083b11e6ea59be9cec758d5
jayjang0209 <jayjang0209@gmail.com>
2024-01-30 16:09:03 -0800

    edit dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 export default function DashboardLayout({
   children,
@@ -7,7 +9,13 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="h-full relative">
-      {children}
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[99] bg-cyan-400">
+        <Sidebar />
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 }
