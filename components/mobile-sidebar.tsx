aecb5cc29c454c70ee9b45dbf79340f9eb1e3a58
jayjang0209 <jayjang0209@gmail.com>
2024-01-30 16:08:45 -0800

    implement mobile sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+import React from "react";
+import Sidebar from "./sidebar";
+
+export default function MobileSidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
