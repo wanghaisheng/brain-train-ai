47c08ecf0fe0a8eefce9c9159b79b07f55890dcf
jayjang0209 <jayjang0209@gmail.com>
2024-02-02 09:37:11 -0800

    add Loading and Empty

--- app/(dashboard)/(routes)/zen-quotes/page.tsx
+++ app/(dashboard)/(routes)/zen-quotes/page.tsx
@@ -5,6 +5,8 @@ import Heading from "@/components/heading";
 import toast from "react-hot-toast";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import Loader from "@/components/loader";
+import Empty from "@/components/empty";
 
 interface Quote {
   quote: string;
@@ -12,9 +14,8 @@ interface Quote {
 }
 
 export default function ZenQuotes() {
-  // Example quote and author. Replace with dynamic data if needed.
   const [quotes, setQuotes] = React.useState<Quote[]>([]);
-  const isLoading = false;
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const onsubmit = async () => {
     try {
@@ -28,6 +29,8 @@ export default function ZenQuotes() {
       setQuotes((prevQuotes) => [...prevQuotes, newQuote]);
     } catch (error) {
       toast.error("An error occurred. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -37,7 +40,7 @@ export default function ZenQuotes() {
       <div className="mx-8 space-y-4 px-4 lg:px-8 bg-indigo-50 mb-6 py-4 border rounded-xl">
         <h3 className="text-2xl font-bold text-indigo-400">Instructions</h3>
         <p className="text-muted-foreground ">
-          Click the button below to see a Zen quote.
+          Click the Generate button below to see a Zen quote.
         </p>
       </div>
       <div className="px-4 lg:px-8">
@@ -55,11 +58,24 @@ export default function ZenQuotes() {
       </div>
 
       <div className="space-y-4 mx-8 my-6 px-4 lg:px-8">
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
+          </div>
+        )}
+        {quotes.length == 0 && !isLoading && (
+          <Empty imageUrl="/quote.svg" label="No quote generated." />
+        )}
         {quotes.map((quote, index) => (
-          <blockquote key={index} className="p-4 italic border-l-4 bg-neutral-100 text-neutral-600 border-indigo-400 quote">
-          <p className="mb-2">{quote.quote}</p>
-          <cite className="block text-right font-semibold">— {quote.author}</cite>
-        </blockquote>
+          <blockquote
+            key={index}
+            className="p-4 italic border-l-4 bg-neutral-100 text-neutral-600 border-indigo-400 quote"
+          >
+            <p className="mb-2">{quote.quote}</p>
+            <cite className="block text-right font-semibold">
+              — {quote.author}
+            </cite>
+          </blockquote>
         ))}
       </div>
     </div>
