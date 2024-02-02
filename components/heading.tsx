dfa56ad3ff862dde029c57c466c23969a89afc61
jayjang0209 <jayjang0209@gmail.com>
2024-02-01 21:37:22 -0800

    restyle heading

--- components/heading.tsx
+++ components/heading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Heading({ children }: { children: React.ReactNode }) {
   return (
-    <div className="cyan_gradient inline-block p-2 min-w-[200px]">
+    <div className="cyan_gradient inline-block min-w-[200px] px-4 lg:px-8">
       <h1 className="text-4xl font-bold mb-5">
         {children}
       </h1>
