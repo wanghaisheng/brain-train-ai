adf272dd4e2a320989b38b5ec1e905d1a398f414
jayjang0209 <jayjang0209@gmail.com>
2024-02-02 11:34:23 -0800

    style dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,10 +5,7 @@ import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 import React from "react";
-import {
-  Music,
-  HeartPulse,
-} from "lucide-react";
+import { Music, HeartPulse } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Card } from "@/components/ui/card";
@@ -46,11 +43,14 @@ export default function DashboardPage() {
   return (
     <div>
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Dashboard
-        </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center max-w-[50rem] mx-auto">
-          Welcome to ZenMelody, where every note is a step towards inner peace
+        <div className="cyan_gradient text-center min-w-[200px] px-4 lg:px-8">
+          <h1 className="text-4xl font-bold mb-5">Dashboard</h1>
+        </div>
+
+        <div className="mx-8 space-y-4 px-4 lg:px-8 bg-indigo-50 mb-6 py-4 border rounded-xl">
+        <h3 className="text-2xl font-bold text-indigo-400">Welcome to ZenMelody</h3>
+        <p className="text-muted-foreground ">
+       every note is a step towards inner peace
           and enhanced concentration. In a world bustling with noise and
           distractions, ZenMelody offers a sanctuary of sound, specially crafted
           to foster relaxation, focus, and mental clarity. Whether you're a
@@ -58,33 +58,35 @@ export default function DashboardPage() {
           tranquility, ZenMelody is your go-to source for auditory bliss.
         </p>
       </div>
-      <div className="px-4 space-y-6 max-w-[600px] mx-auto">
-          {tools.map((tool, index) => (
-            <Card
-              onClick={() => router.push(tool.href)}
-              key={tool.href}
-              className="p-4 border border-black/5 flex flex-col items-center justify-between hover:shadow-md transition cursor-pointer hover:bg-cyan-50/80"
-            >
-              <div className="flex items-center justify-between w-full">
-                <div className="flex items-center gap-x-4">
-                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                    <tool.icon className={cn("w-8 h-8", tool.color)} />
-                  </div>
-                  <div className="font-semibold">{tool.label}</div>
-                </div>
-                <AudioLines className="w-5 h-5" />
-              </div>
-              <div className="relative w-full aspect-video rounded-md overflow-hidden mt-4">
-                <Image
-                  fill
-                  className="object-cover"
-                  alt={tool.label}
-                  src={tool.imageUrl}
-                />
-              </div>
-            </Card>
-          ))}
-        </div>
+
       </div>
+      <div className="px-4 space-y-6 max-w-[600px] mx-auto">
+        {tools.map((tool, index) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border border-black/5 flex flex-col items-center justify-between hover:shadow-md transition cursor-pointer hover:bg-cyan-50/80"
+          >
+            <div className="flex items-center justify-between w-full">
+              <div className="flex items-center gap-x-4">
+                <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                  <tool.icon className={cn("w-8 h-8", tool.color)} />
+                </div>
+                <div className="font-semibold">{tool.label}</div>
+              </div>
+              <AudioLines className="w-5 h-5" />
+            </div>
+            <div className="relative w-full aspect-video rounded-md overflow-hidden mt-4">
+              <Image
+                fill
+                className="object-cover"
+                alt={tool.label}
+                src={tool.imageUrl}
+              />
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
