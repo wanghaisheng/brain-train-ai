329e1bbfc1da7ce39c643043af2e4a64d630eaee
jayjang0209 <jayjang0209@gmail.com>
2024-02-05 16:19:34 -0800

    add getApiLimitCount

--- lib/api-limits.ts
+++ lib/api-limits.ts
@@ -29,7 +29,7 @@ export const incrementApiLimit = async () => {
     // If the user has used the API before, increment the count
     await prismadb.userApiLimit.update({
       where: {
-        userId, 
+        userId,
       },
       data: {
         count: userApiLimit.count + 1,
@@ -56,4 +56,24 @@ export const checkApiLimit = async () => {
   }
 
   return userApiLimit.count < MAX_FREE_COUNTS;
-}
+};
+
+export const getApiLimitCount = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return 0;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!userApiLimit) {
+    return 0;
+  }
+
+  return userApiLimit.count;
+};
