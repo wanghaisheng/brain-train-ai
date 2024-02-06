622bad29ffe3d1a6cda09daca1a6caebd7756959
jayjang0209 <jayjang0209@gmail.com>
2024-02-06 11:37:09 -0800

    enable button click on Subscrib

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import React from "react";
+import React, { useState} from "react";
 import { Dialog, DialogOverlay, DialogContent } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { useProModal } from "@/hooks/pro-modal";
 import { AudioLines } from "lucide-react";
 import { Zap } from "lucide-react";
 import Link from "next/link";
+import axios from "axios";
 
 export default function ProModal() {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe")
+      window.location.href = response.data.url;
+
+    } catch (error: any) {
+      console.error("STRIPE Client Error: ", error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.close}>
@@ -30,7 +45,9 @@ export default function ProModal() {
             Upgrade to unlock premium features and unlimited access to
             ZenMelody.
           </p>
-          <Button className="text-white bg-gradient-to-r from-indigo-400 to-pink-500 hover:bg-indigo-500 hover:scale-110 px-4 py-2 rounded-lg font-bold transition-transform duration-150 ease-in-out">
+          <Button 
+            onClick={onSubscribe}
+          className="text-white bg-gradient-to-r from-indigo-400 to-pink-500 hover:bg-indigo-500 hover:scale-110 px-4 py-2 rounded-lg font-bold transition-transform duration-150 ease-in-out">
             Unlock Pro
             <Zap className="h-5 w-5 ml-1" />
           </Button>
