bbcadb95ef5b2a45a2928eb17b23e59e478dcf0f
jayjang0209 <jayjang0209@gmail.com>
2024-02-06 12:32:55 -0800

    edit Sidebar style

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,10 +8,10 @@ import FreeCounter from "./free-counter";
 
 export default function Sidebar({ apiLimitCount }: { apiLimitCount: number }) {
   return (
-    <div className="flex flex-col h-full border-r bg-white shadow-sm pt-3">
-      <div className="flex flex-col space-y-4 py-4 flex-grow">
+    <div className="flex flex-col h-full border-r bg-white shadow-sm">
+      <div className="flex flex-col space-y-2 py-2 flex-grow">
         <div className="px-3 py-2">
-          <Link href="/dashboard" className="flex items-center pl-3 mb-4 mt-3">
+          <Link href="/dashboard" className="flex items-center pl-3 mb-4">
             <div className="flex items-center mr-2">
               <AudioLines className="h-10 w-10 text-cyan-400" />
             </div>
