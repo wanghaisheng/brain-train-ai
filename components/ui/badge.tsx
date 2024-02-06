922ded77bb7045ea0515a4b32fad61e63f733534
jayjang0209 <jayjang0209@gmail.com>
2024-02-05 16:29:32 -0800

    install badge ui

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -15,6 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        accent: "border-transparent bg-indigo-400 text-primary-foreground hover:bg-primary/80"
       },
     },
     defaultVariants: {
